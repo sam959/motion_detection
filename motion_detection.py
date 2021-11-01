b938ff998d209321fca2ab00e075be575c4a78ed
sam959 <liekemonster@gmail.com>
2021-11-02 00:52:32 +0100

    Show colored window, update threshold of delta processing

--- motion_detection.py
+++ motion_detection.py
@@ -19,19 +19,17 @@ baseFrame = None
 def getThreshold():
     return cv.getTrackbarPos("Threshold", "Thresh")
 
-
 def grabFirstFrame(frame):
     global baseFrame
     if baseFrame is None:
         print('Grabbing first frame...')
         baseFrame = frame
 
-
 def grabCurrentFrameForNextComparison(frame):
     global baseFrame
     baseFrame = frame
 
-def detectMotion(frame):
+def detectMotion(frame, colored):
     global baseFrame
     # compute the absolute difference between the current frame and
 	# first frame
@@ -42,7 +40,7 @@ def detectMotion(frame):
     displayFrame = frame.copy()
 
     frameDelta = cv.absdiff(baseFrame, frame)
-    thresh = cv.threshold(frameDelta, 25, 255, cv.THRESH_BINARY)[1]
+    thresh = cv.threshold(frameDelta, 55, 255, cv.THRESH_BINARY)[1]
 
     dilated = cv.dilate(thresh, None, iterations=3)
 
@@ -61,26 +59,22 @@ def detectMotion(frame):
   
         if cv.contourArea(c) < getThreshold():
             continue
-        # if the contour area is larger than our supplied --min-area , we’ll draw the bounding box 
+        # if the contour area is larger, we’ll draw the bounding box 
         # surrounding the foreground and motion region
-        cv.rectangle(displayFrame, (x, y), (x + w, y + h), (255, 255, 255), 2)
-        cv.putText(displayFrame, "Status: {}".format('Movement'), (10, frame.shape[0] - 10), cv.FONT_HERSHEY_SIMPLEX,
+        cv.rectangle(colored, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv.putText(colored, "Status: {}".format('Movement'), (10, frame.shape[0] - 10), cv.FONT_HERSHEY_SIMPLEX,
             1, (0, 0, 255), 2)
         cv.putText(thresh, "Threshold: {}".format(getThreshold()), (10, frame.shape[0] - 10), cv.FONT_HERSHEY_SIMPLEX,
             1, (0, 0, 255), 2)
 
-
         print('Definging bounding box...')
 
     #cv.drawContours(frame, contours, -1, (0,255,0), 3)
-    canvas = np.hstack((thresh, frameDelta, displayFrame))
-    cv.imshow('Threshold, FrameDelta, Source', canvas)
-    #cv.imshow("Thresh", thresh)
-    #cv.imshow("Delta", frameDelta)
-    #cv.imshow("Feed", displayFrame)
-
-
-
+    #canvas = np.hstack((thresh, frameDelta, displayFrame))
+    #cv.imshow('Threshold, FrameDelta, Source', canvas)
+    cv.imshow("Thresh", thresh)
+    cv.imshow("Delta", frameDelta)
+    cv.imshow("Feed", colored)
 
 def main():
     video = cv.VideoCapture(0)
@@ -93,20 +87,9 @@ def main():
         resized = cv.resize(frame, (640, 480), interpolation= cv.INTER_AREA)
         gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
         gray = cv.GaussianBlur(gray, (3, 3), 0)
-       
-        detectMotion(gray)
+        
+        detectMotion(gray, resized)
         grabCurrentFrameForNextComparison(gray)
-
-        if cv.waitKey(20) & 0xFF == ord("s"):
-            cv.destroyAllWindows()
-            print("Initializing selection...")
-            if cv.waitKey(20) & 0xFF == ord('q'):
-                break
-            r = cv.selectROI("Selection", canvas, False, False)
-            print("Image slected!")
-            canvas = canvas[int(r[1]) : int(r[1] + r[3]), int(r[0]) : int(r[0] + r[2])]
-            cv.destroyAllWindows()
-            cv.imshow("Cropped", canvas)
 
         if cv.waitKey(20) & 0xFF == ord('q'):
             break
